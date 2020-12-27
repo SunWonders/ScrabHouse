adbfe60a54a57e75a281fc164a7a3f5dbf3b3d60
Dilip D <dilipduraiswamy@gmail.com>
2020-12-27 21:00:36 +0530

    Updating Port Number

--- src/main/java/com/sunwonders/trashman/TrashManApplication.java
+++ src/main/java/com/sunwonders/trashman/TrashManApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.sunwonders.trashman.util.MailSender;
-
 // TODO: Auto-generated Javadoc
 /**
  * The Class TrashManApplication.
